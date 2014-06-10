dd76573a0a70e1ac34fa77e991744e623744bd72
danwaters <dwaters@gmail.com>
2014-06-10 15:21:11 -0700

    Initial commit

---
+++ AdaptiveContentSample/AdaptiveContentPage.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using Xamarin.Forms;
+
+namespace AdaptiveContentSample
+{
+	public class AdaptiveContentPage : ContentPage
+	{
+		public AdaptiveContentPage (View portraitLayout, View landscapeLayout)
+		{
+			PortraitLayout = portraitLayout;
+			LandscapeLayout = landscapeLayout;
+
+			this.SizeChanged += HandleSizeChanged;
+		}
+
+		public View PortraitLayout { get; private set; }
+		public View LandscapeLayout { get; private set; }
+
+		void HandleSizeChanged (object sender, EventArgs e)
+		{
+			this.Content = Width > Height ? LandscapeLayout : PortraitLayout;
+		}
+			
+		public void SetLayouts(View portraitLayout, View landscapeLayout)
+		{
+			this.PortraitLayout = portraitLayout;
+			this.LandscapeLayout = landscapeLayout;
+		}
+	}
+}
+
---
+++ AdaptiveContentSample/App.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using Xamarin.Forms;
+
+namespace AdaptiveContentSample
+{
+	public class App
+	{
+		public static Page GetMainPage ()
+		{	
+			var portraitLayout = new ContentView {
+				Content = new StackLayout {
+					Orientation = StackOrientation.Vertical,
+					HorizontalOptions = LayoutOptions.CenterAndExpand,
+					VerticalOptions = LayoutOptions.CenterAndExpand,
+					Children = { 
+						new Label { Text = "First" }, 
+						new Label { Text = "Second" }, 
+						new Label { Text = "Third" } 
+					}
+				}
+			};
+
+			var landscapeLayout = new ContentView {
+				Content = new StackLayout {
+					HorizontalOptions = LayoutOptions.CenterAndExpand,
+					VerticalOptions = LayoutOptions.CenterAndExpand,
+					Orientation = StackOrientation.Horizontal,
+					Children = { 
+						new Label { Text = "First" }, 
+						new Label { Text = "Second" }, 
+						new Label { Text = "Third" } 
+					}
+				}
+
+			};
+				
+			return new AdaptiveContentPage (portraitLayout, landscapeLayout);
+		}
+	}
+}
+
